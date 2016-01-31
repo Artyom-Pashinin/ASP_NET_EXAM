925b4f811836246907f3656fb876b9bd29d91ea9
Artyom-Pashinin <artyom.pashinin@gmail.com>
2016-02-01 00:45:40 +0300

    Финальные изменения
    Добавлены  и исправлены модели и контроллеры. Финальная версия сайта

---
+++ Exam-2014-NewsSite/Models/AppUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using NewsSite.Models;
+
+namespace NewsSite.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public ClaimsIdentity GenerateUserIdentity(ApplicationUserManager manager)
+        {
+            var userIdentity = manager.CreateIdentity(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+
+        public Task<ClaimsIdentity> GenerateUserIdentityAsync(ApplicationUserManager manager)
+        {
+            return Task.FromResult(GenerateUserIdentity(manager));
+        }
+    }
+}
